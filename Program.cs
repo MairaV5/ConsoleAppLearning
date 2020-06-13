d19eece242c2a6e2d69cb18e84ed365ad8ab93b4
Maira Vecele <mairavecele6@gmail.com>
2020-06-13 18:00:05 +0300

    version 18:00

--- Program.cs
+++ Program.cs
@@ -7,14 +7,16 @@ namespace ConsoleAppLearning
         static void Main(string[] args)
         {
             Human person = new Human();
-            person.Name = "Josh";
-            person.Surname = "Lopez";
+            //person.Name = "Josh";
+            //person.Surname = "Lopez";
             person.Age = 17;
             person.EyeColor = "Blue";
             person.Introduce();
 
-            person.Name = "James";
+            //person.Name = "James";
             person.Introduce();
+
+            person.AddNumbers(32, 968);
         }
     }
 }
