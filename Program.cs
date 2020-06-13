391792133ff4a31121353844737da13cc4a2735b
Maira Vecele <mairavecele6@gmail.com>
2020-06-13 17:44:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person = new Human();
+            person.Name = "Josh";
+            person.Surname = "Lopez";
+            person.Age = 17;
+            person.EyeColor = "Blue";
+            person.Introduce();
+
+            person.Name = "James";
+            person.Introduce();
+        }
+    }
+}
